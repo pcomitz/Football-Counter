46824bbe1a92fa6db4277ad5ad318330c5d7bdf4
pcomitz <pcomitz@gmail.com>
2016-07-17 14:13:39 -0400

    Sunday 7/17 mysterious "critcial" issue.  I deleted a few blank lines. There are no unused variables.

--- app/src/main/java/com/example/android/footballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballcounter/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimpSlifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
